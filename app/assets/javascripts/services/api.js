5a3cdda1b6013049f0ab6727e39c84f7f8aee3a6
Gus Bonfant <gbonfant@me.com>
2014-08-16 21:30:13 +0200

    use api-release endpoint

--- app/assets/javascripts/services/api.js
+++ app/assets/javascripts/services/api.js
@@ -6,9 +6,7 @@
   Api.$inject = ['$http'];
 
   function Api($http) {
-    // this.url = 'http://api-dev.farmdrop.co.uk/api/v1/';
     this.url = 'https://api-release.farmdrop.co.uk/api/v1/';
-    // this.url = 'https://api.farmdrop.co.uk/api/v1/';
 
     this.getDrops = function() {
       return $http.get(this.url + 'drops');
