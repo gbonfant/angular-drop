efd55e69d6529081c9dd9d69d187a3a644d9a00a
Gus Bonfant <gbonfant@me.com>
2014-08-17 11:42:56 +0200

    editing actions for drop details ctrl

--- app/assets/javascripts/controllers/drop_details_controller.js
+++ app/assets/javascripts/controllers/drop_details_controller.js
@@ -9,7 +9,31 @@
     //
     // Model data
     //
-    this.drop = undefined;
+    this.drop     = undefined;
+    this.newDrop  = undefined;
+    this.showForm = false;
+
+    //
+    // Actions
+    //
+    this.toggleForm = function() {
+      this.showForm = !this.showForm;
+    };
+
+    this.enterEdit = function() {
+      if (this.newDrop) {
+        return;
+      }
+
+      this.newDrop = angular.copy(this.drop);
+    };
+
+
+    this.updateDrop = function() {
+      Api.updateDrop(this.newDrop).success(function(response) {
+        // updated
+      });
+    };
 
     //
     // Initialisation calls
--- specs/controllers/drop_details_controller_spec.js
+++ specs/controllers/drop_details_controller_spec.js
@@ -23,5 +23,24 @@
     it('should set drop response on controller initialisation', function() {
       expect(dropDetailsCtrl.drop).toEqual({ id: 1 });
     });
+
+    it('should set a showForm attribute with defaults', function() {
+      expect(dropDetailsCtrl.showForm).toBe(false);
+    });
+
+    describe('toggleForm()', function() {
+      it('should set showForm to true', function() {
+        dropDetailsCtrl.toggleForm();
+
+        expect(dropDetailsCtrl.showForm).toBe(true);
+      });
+
+      it('should set showForm to false', function() {
+        dropDetailsCtrl.toggleForm();
+        dropDetailsCtrl.toggleForm();
+
+        expect(dropDetailsCtrl.showForm).toBe(false);
+      });
+    });
   });
 })();
